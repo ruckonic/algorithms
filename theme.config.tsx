d9034bf06c31b3a5040839eec470b31bd7d57c91
Rene Corrales <ruckonic@icloud.com>
2023-06-13 15:49:49 -0600

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <div>ruckonic.dev</div>,
+  project: {
+    link: 'https://github.com/ruckonic/algorithms',
+  },
+  docsRepositoryBase: 'https://github.com/ruckonic/algorithms',
+  logoLink: 'https://ruckonic.dev'
+}
+
+export default config
