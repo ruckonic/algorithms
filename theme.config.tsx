dbe9785c3a1c26ecfe4e8dbbce0d93f85176e095
Rene Corrales <ruckonic@icloud.com>
2023-06-13 16:26:54 -0600

    feat: set tabs names

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,6 +5,12 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://github.com/ruckonic/algorithms',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s | ruckonic.dev',
+      defaultTitle: 'ruckonic.dev',
+    }
+  },
   docsRepositoryBase: 'https://github.com/ruckonic/algorithms',
   logoLink: 'https://ruckonic.dev'
 }
